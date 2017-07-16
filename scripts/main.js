86614f9301fab181995df0850138ce8ad7c96842
John Roche <roche.johnjr@gmail.com>
2017-07-16 19:51:08 -0400

    Stuck on next steps

--- scripts/main.js
+++ scripts/main.js
@@ -9,69 +9,80 @@
    write your logic.
 */
 
-let hand = [
-  {
-    card: '2', // [0]
-    value: 2,
-  },
-  {
-    card: '3', // [1]
-    value: 3,
-  },
-  {
-    card: '4', // [2]
-    value: 4,
-  },
-  {
-    card: '5', // [3]
-    value: 5,
-  },
-  {
-    card: '6', // [4]
-    value: 6,
-  },
-  {
-    card: '7', // [5]
-    value: 7,
-  },
-  {
-    card: '8', // [6]
-    value: 8,
-  },
-  {
-    card: '9', // [7]
-    value: 9,
-  },
-  {
-    card: '10', // [9]
-    value: 10,
-  },
-  {
-    card: 'J', // [9]
-    value: 10,
-  },
-  {
-    card: 'Q', // [10]
-    value: 10,
-  },
-  {
-    card: 'K', // [11]
-    value: 10,
-  },
-  {
-    card: 'A', // [12]
-    value: [1, 11]
+const cards = {
+	details: [
+		{
+			card: '2', // [0]
+			value: 2
+		},
+		{
+			card: '3', // [1]
+			value: 3
+		},
+		{
+			card: '4', // [2]
+			value: 4
+		},
+		{
+			card: '5', // [3]
+			value: 5
+		},
+		{
+			card: '6', // [4]
+			value: 6
+		},
+		{
+			card: '7', // [5]
+			value: 7
+		},
+		{
+			card: '8', // [6]
+			value: 8
+		},
+		{
+			card: '9', // [7]
+			value: 9
+		},
+		{
+			card: '10', // [9]
+			value: 10
+		},
+		{
+			card: 'J', // [9]
+			value: 10
+		},
+		{
+			card: 'Q', // [10]
+			value: 10
+		},
+		{
+			card: 'K', // [11]
+			value: 10
+		},
+		{
+			card: 'A', // [12]
+			value: [1, 11]
+		}
+	]
+};
+
+// test
+
+console.log('loaded JS');
+const hand = cards.details;
+console.log(`I have ${hand.length} cards`);
+
+// create function
+
+function handValue(blackJackHand) {
+
+// iterate through array
+
+  for (let i = 0; i < hand.length; i++) {
+    
   }
-]
 
-
-
-function handValue (hand) {
-
-
-  return;
 }
-
 
 /* -----  Hints ------
 
