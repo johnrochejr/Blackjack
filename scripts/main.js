f3f2e15c916afcf0aef31e609466404c7b0572fe
John Roche <roche.johnjr@gmail.com>
2017-07-16 18:16:13 -0400

    added array

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,62 @@
    write your logic.
 */
 
+let hand = [
+  {
+    card: '2', // [0]
+    value: 2,
+  },
+  {
+    card: '3', // [1]
+    value: 3,
+  },
+  {
+    card: '4', // [2]
+    value: 4,
+  },
+  {
+    card: '5', // [3]
+    value: 5,
+  },
+  {
+    card: '6', // [4]
+    value: 6,
+  },
+  {
+    card: '7', // [5]
+    value: 7,
+  },
+  {
+    card: '8', // [6]
+    value: 8,
+  },
+  {
+    card: '9', // [7]
+    value: 9,
+  },
+  {
+    card: '10', // [9]
+    value: 10,
+  },
+  {
+    card: 'J', // [9]
+    value: 10,
+  },
+  {
+    card: 'Q', // [10]
+    value: 10,
+  },
+  {
+    card: 'K', // [11]
+    value: 10,
+  },
+  {
+    card: 'A', // [12]
+    value: [1, 11]
+  }
+]
+
+
 
 function handValue (hand) {
 
